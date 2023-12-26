ef95c82c6d41fe031e3bea7102dd74253b6372c7
Harchit Bhatoia <harchit@Harchits-Laptop.local>
2023-12-26 14:51:05 -0500

    prjects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,6 +5,9 @@ export default function Projects() {
   return (
     <div class="projects">
         
+        <a href="https://reverb-96eb6668d40c.herokuapp.com/" target="_blank"><b>Reverb</b></a>
+        <p>social media site for musicians (MERN stack, MaterialUI)
+        <br/>user sign up with friends and content feed</p>
         <a href="https://harchit.github.io/sortingalgo/" target="_blank"><b>sorting visualizer</b></a>
         <p>visualize common sorting algorithms on integer arrays (React)</p>
         <br/>
@@ -14,10 +17,6 @@ export default function Projects() {
         <a href="https://github.com/harchit/PS5_tracker" target="_blank"><b>PS5 tracker</b></a>
         <p>sends a text message when the PS5 is in stock on Amazon.
         <br/>uses python's beautiful soup to scrape data</p>
-        <br/>
-        <a href="https://reverb-96eb6668d40c.herokuapp.com/" target="_blank"><b>Reverb</b></a>
-        <p>social media site for musicians (MERN stack, MaterialUI)
-        <br/>user sign up with friends and content feed</p>
         <a href="/"><b>personal website</b></a>
         <p>the site you're currently viewing (React)<br/> 
         </p>
